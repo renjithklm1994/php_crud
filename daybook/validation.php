424e6c7178b8bb4557cc85e9a109c585d5a54213
renjithklm1994 <46755547+renjithklm1994@users.noreply.github.com>
2023-06-25 17:20:52 +0530

    validation added

--- daybook/validation.php
+++ daybook/validation.php
@@ -1,20 +1,25 @@
 <?php
-function validaton_db( $name, $email, $phone,$reg_number,$gender,$course, $errors
-)
-{ $valid = true;
+function validaton_db( $name, $email, $phone,$reg_number,$gender,$course, $errors)
+{
+     $valid = true;
     
     if (empty($name)) {
         $errors[] = "Name is required.";
+        $valid = false;
+        } elseif(!preg_match('/^[a-zA-Z]+$/',$name)){
+        $errors[] ="invalid Name Format";
+        $valid = false;
+         }  
+
+    if (empty($email)) {
+        $errors[] = "Email is required.";
         $valid = false; 
-    } else {
-        if (empty($email)) {
-            $errors[] = "Email is required.";
-            $valid = false; 
-        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errors[] = "Invalid email format.";
-            $valid = false; 
-        }
     }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+         $errors[] = "Invalid email format.";
+         $valid = false; 
+    }
+    
 
     if (empty($phone)) {
         $errors[] = "Phone number is required.";
@@ -26,6 +31,9 @@ function validaton_db( $name, $email, $phone,$reg_number,$gender,$course, $error
 
     if (empty($reg_number)) {
         $errors[] = "Registration number is required.";
+        $valid = false; 
+    }elseif (!preg_match('/^\d{10}$/', $reg_number)) {
+        $errors[] = "Invalid registration number format.";
         $valid = false; 
     }
 
@@ -49,5 +57,4 @@ function validaton_db( $name, $email, $phone,$reg_number,$gender,$course, $error
 
     return $valid;
 }
-
 ?>
